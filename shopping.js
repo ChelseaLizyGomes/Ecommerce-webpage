307de6124cb1b49586af34fd344319f9020a6e30
Chelsea Gomes <chelsealizygomes11@gmail.com>
2024-05-06 15:40:30 +0530

    added new changes

--- shopping.js
+++ shopping.js
@@ -2,26 +2,50 @@ const product = [
     {
         id: 0,
         image: "1.jpg",
-        title: 'round diamond earrings',
+        title: 'Round diamond earrings',
         price: 1200,
     },
     {
         id: 1,
         image: '2.jpg',
-        title: 'long diamond earrings',
+        title: 'Long diamond earrings',
         price: 7000,
     },
     {
         id: 2,
         image: '3.jpg',
-        title: 'red ruby earrings',
+        title: 'Red ruby earrings',
         price: 1300,
     },
     {
         id: 3,
         image: '4.png',
-        title: 'red gem earrings',
+        title: 'Red gem earrings',
         price: 2999,
+    },
+    {
+        id: 4,
+        image: "5.png",
+        title: 'Yellow casual summer dress',
+        price: 400,
+    },
+    {
+        id: 5,
+        image: '6.jpg',
+        title: 'Black pointed high heels',
+        price: 900,
+    },
+    {
+        id: 6,
+        image: '7.png',
+        title: 'Pink leather bag',
+        price: 1200,
+    },
+    {
+        id: 3,
+        image: '8.jpg',
+        title: 'Brown high-rise formal pants',
+        price: 799,
     }
 ];
 //to display on page
@@ -32,7 +56,7 @@ let i = 0;
 document.getElementById('root').innerHTML = categories.map((item) => {
     var { image, title, price } = item; //locally defining what will consititute the item.
     return (
-        `<div class='box'>
+        ` <div class='box'>
 <div class='img-box'>
 <img class= 'images' src= ${image}></img>
 </div>
@@ -41,31 +65,47 @@ document.getElementById('root').innerHTML = categories.map((item) => {
 <h4>Rs.${price}.00</h4>` +
         "<button onclick='addtocart(" + (i++) + ")'>Add to cart</button>" +
         `</div>
-</div>`
+</div> `
     )
 }).join('')
 
-var cart = [];
+var mycart = [];
+
+var insideCart = (`<div class="sidebar">
+<div class="head">
+    <p>My Cart</p>
+    <!--<p id="count">0</p>-->
+</div>
+
+    <div id="cartItem">Your cart is empty</div>
+    <div class="foot">
+        <h3>Total</h3>
+        <h2 id="total">$ 0.00</h2>
+    </div>
+</div>`);
 
 function addtocart(a) {
-    cart.push({ ...categories[a] });
+    mycart.push({ ...categories[a] });
     displaycart();
 }
 
 function delElement(a) {
-    cart.splice(a, 1);
+    mycart.splice(a, 1);
     displaycart();
 }
+document.getElementById("inside").innerHTML = insideCart;
+
 function displaycart(a) {
+    insideCart = insideCart;
     let j = 0;
     total = 0;
-    document.getElementById("count").innerHTML = cart.length;
-    if (cart.length == 0) {
+    document.getElementById("count").innerHTML = mycart.length;
+    if (mycart.length == 0) {
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
         document.getElementById("total").innerHTML = "$" + 0 + "00";
     }
     else {
-        document.getElementById("cartItem").innerHTML = cart.map((items) => {
+        document.getElementById("cartItem").innerHTML = mycart.map((items) => {
             var { image, title, price } = items;
             total = total + price;
             document.getElementById("total").innerHTML = "$" + total + ".00";
@@ -81,5 +121,10 @@ function displaycart(a) {
         }).join('');
     }
 }
+submit.addEventListner('click', displaycart);
 
+/* Example usage: Add a product to the cart on button click
+document.querySelector('.addtocart').addEventListener('click', function() {
+  addToCart(a); // Add the product with id 0 to the cart
+});  */
 
